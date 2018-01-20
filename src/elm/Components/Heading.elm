22deb3e5f169a955368af2aacd99f1102db37ef9
jen-harris <jennifer.louise.harris@gmail.com>
2018-01-20 21:12:27 +0000

    finishes styling on heading

--- src/elm/Components/Heading.elm
+++ src/elm/Components/Heading.elm
@@ -11,6 +11,7 @@ headingComponent ( title, para1, para2 ) =
         [ h2 [ class "tc f3 pt5 fw4" ] [ text title ]
         , p [ class "f4 w50 mh1 tc fw1" ]
             [ text para1 ]
-        , p [ class "f4 w50 mh1 tc fw1" ]
+        , p [ class "f4 w50 mh1 tc fw1 pb5" ]
             [ text para2 ]
+        , div [ class "bb b--gray w-100 tc pa2" ] []
         ]
