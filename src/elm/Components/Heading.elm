9d8e2683a80b685a47d322106b3e4b2d540682d8
jen-harris <jennifer.louise.harris@gmail.com>
2018-01-21 19:02:40 +0000

    alters styling to vh100 and buttons on images

--- src/elm/Components/Heading.elm
+++ src/elm/Components/Heading.elm
@@ -7,11 +7,10 @@ import Types exposing (..)
 
 headingComponent : ( String, String, String ) -> Html Msg
 headingComponent ( title, para1, para2 ) =
-    section [ class "w-50-ns center pa4 pa2-ns " ]
+    section [ class "w-30-ns center pa4 pa2-ns vh-100 v-mid dtc" ]
         [ h2 [ class "tc f3 pt5 fw4" ] [ text title ]
         , p [ class "f4 w50 mh1 tc fw1" ]
             [ text para1 ]
         , p [ class "f4 w50 mh1 tc fw1 pb5" ]
             [ text para2 ]
-        , div [ class "bb b--gray w-100 tc pa2" ] []
         ]
--- src/elm/Routes/Home.elm
+++ src/elm/Routes/Home.elm
@@ -16,13 +16,25 @@ home model =
             , "I'm a UX/UI designer & developer and recent Hong Kong Expat."
             , "Welcome to my blog."
             )
-        , section [ class "db tc mv4 " ]
-            [ a [ href "#hongKong" ] [ img [ src "./assets/mainHkImage.png", class "tc mw7-m ma4 dib w-90" ] [] ]
-            , div [ class "bb b--gray w-50-m w-90 dib" ] []
+        , section [ class "db tc mv4 no-underline" ]
+            [ a
+                [ href "#hongKong"
+                , class "hkMain vh-100 dt  w-100 tc center cover no-underline"
+                ]
+                [ div [ class "v-mid dtc grow" ]
+                    [ a [ href "#hongKong", class "f2 fw4 white ba b--white pa3 no-underline bw2 bg-black-20 b" ] [ text "Hong Kong" ]
+                    ]
+                ]
             ]
-        , section [ class "db tc mt4" ]
-            [ a [ href "#design" ] [ img [ src "./assets/mainDesignImage.png", class "tc mw7-m ma4 dib w-90" ] [] ]
-            , div [ class "bb b--gray w-75-m w-50-l w-90 dib " ] []
+        , section [ class "db tc mv4 no-underline" ]
+            [ a
+                [ href "#design"
+                , class "designMain vh-100 dt  w-100 tc center cover no-underline"
+                ]
+                [ div [ class "v-mid dtc grow" ]
+                    [ a [ href "#design", class "f2 fw4 white ba b--white pa3 no-underline bw2 bg-black-20 b" ] [ text "Design & Tech" ]
+                    ]
+                ]
             ]
         , section []
             (List.map
