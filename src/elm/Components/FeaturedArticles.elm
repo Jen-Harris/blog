653df30df71519e181060021e607ce7bb11c80fb
jen-harris <jennifer.louise.harris@gmail.com>
2018-02-23 15:38:43 +0000

    corrects issues with borders etc now ready for articles to be inputted

--- src/elm/Components/FeaturedArticles.elm
+++ src/elm/Components/FeaturedArticles.elm
@@ -7,8 +7,11 @@ import Types exposing (..)
 
 featuredArticles : FeaturedArticles -> Html Msg
 featuredArticles { post, photo, title, date, summary, route } =
-    div [ class "justify-between-ns dtc center pt4" ]
-        [ img [ src photo, class "mw6-ns w-80" ] []
+    a [ href ("#" ++ route), class "dtc center ba shadow-3 b--gray w-30-ns w-90 mb4 no-underline dark-gray" ]
+        [ img [ src photo, class "w-100 ma0 pa0 w-80" ] []
         , br [] []
-        , text "hello"
+        , div [ class "ph3" ]
+            [ h2 [ class "b fw4" ] [ text title ]
+            , p [] [ text summary ]
+            ]
         ]
--- src/elm/Data/FeaturedAriclesDesignList.elm
+++ src/elm/Data/FeaturedAriclesDesignList.elm
@@ -19,4 +19,11 @@ featuredArticlesDesignList =
       , summary = "What is Founders and Coders? Why did I do it? Should you do it too?"
       , route = "founders"
       }
+    , { post = "Hong Kong"
+      , photo = "./assets/mainHkImage.png"
+      , title = "Moving to Hong Kong"
+      , date = "23.01.18"
+      , summary = "It's actually happening. We're moving to Hong Kong."
+      , route = "moving"
+      }
     ]
