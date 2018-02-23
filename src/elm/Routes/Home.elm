420add3ed3a727231dd48b4a7098850d19256e67
jen-harris <jennifer.louise.harris@gmail.com>
2018-02-23 12:57:34 +0000

    adds new sizing to images on home page

--- src/elm/Routes/Home.elm
+++ src/elm/Routes/Home.elm
@@ -17,10 +17,10 @@ home model =
             , "I'm a UX/UI designer & developer and recent Hong Kong Expat."
             , "Welcome to my blog."
             )
-        , section [ class "db tc mv4 no-underline" ]
+        , section [ class "db tc mb4 no-underline" ]
             [ a
                 [ href "#hongKong"
-                , class "hkMain vh-100 dt  w-100 tc center cover no-underline"
+                , class "hkMain vh-75 dt  w-90 tc center cover no-underline"
                 ]
                 [ div [ class "v-mid dtc grow" ]
                     [ a [ href "#hongKong", class "f2 fw4 white ba b--white pa3 no-underline bw2 bg-black-20 b" ] [ text "Hong Kong" ]
@@ -30,7 +30,7 @@ home model =
         , section [ class "db tc mv4 no-underline" ]
             [ a
                 [ href "#design"
-                , class "designMain vh-100 dt  w-100 tc center cover no-underline"
+                , class "designMain vh-75 dt  w-90 tc center cover no-underline"
                 ]
                 [ div [ class "v-mid dtc grow" ]
                     [ a [ href "#design", class "f2 fw4 white ba b--white pa3 no-underline bw2 bg-black-20 b" ] [ text "Design & Tech" ]
